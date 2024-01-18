adc7df39e113c73d11c597947345cce0e06c3a6f
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-18 16:28:34 +0100

    add: some comments

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const difficulty = document.getElementById("difficulty");
 let valueDifficulty;
 console.log(valueDifficulty);
 genera.addEventListener('click', function(){
+    //*svuoto la glriglia
+    boxContainer.innerHTML = " ";
+    //* converto in intero il value della difficolta
     valueDifficulty = parseInt(difficulty.value);
-    boxGenerator(); 
+    //* call all funzione per generare le box
+    boxGenerator();
+    //* call della funzione griglia 
     generateGrid (boxContainer ,valueDifficulty );
     
-    console.log(valueDifficulty);
 })
 
 
@@ -17,6 +21,7 @@ genera.addEventListener('click', function(){
 
 //** box generator  */
 function boxGenerator() {
+    //* vreo l'el e aggiungo la classe box
     const box = document.createElement('div');
     box.classList.add ('box');
     return box;
@@ -24,10 +29,16 @@ function boxGenerator() {
 //** Grid generator */
 function
 generateGrid (boxContainer , difficulty) {
+    //* calc della radice del value della diffioclta
     let x = Math.sqrt(difficulty)
+    
+    //* creo la classe da usare per quella diffioclta 
     let y = "container-" + x ;
+
+    //* aggiungo la classe al boxContainer
     boxContainer.classList.toggle(y)
-    console.log(x);
+
+    //* aggingo l'el box nel boxContainer
     for (let i = 0; i < difficulty; i++) {
     const boxElement = boxGenerator();
     boxContainer.append(boxElement);
