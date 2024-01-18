2045c98cdfcfe16ad91ba12a7a3e009e9dc465d4
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-18 18:05:35 +0100

    fix : bug su remove container old class calss

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const boxContainer = document.getElementById("boxContainer");
 const genera = document.getElementById("genera");
 const difficulty = document.getElementById("difficulty");
-
+let oldContainerClass;
 let valueDifficulty;
 
 genera.addEventListener('click', function(){
     //*svuoto la glriglia
     boxContainer.innerHTML = " ";
+    
     //* converto in intero il value della difficolta
     valueDifficulty = parseInt(difficulty.value);
+    
+    //* call della funzione griglia 
+    oldContainerClass = generateGrid (boxContainer ,valueDifficulty, oldContainerClass );
+
     //* call all funzione per generare le box
     boxGenerator();
-    //* call della funzione griglia 
-    generateGrid (boxContainer ,valueDifficulty );
-   
+    
     
 });
-
 
 
 
@@ -27,6 +29,7 @@ genera.addEventListener('click', function(){
 
 //** box generator  */
 function boxGenerator(nr) {
+    
     //* vreo l'el e aggiungo la classe box
     const box = document.createElement('div');
     box.classList.add ('box');
@@ -35,27 +38,28 @@ function boxGenerator(nr) {
         box.classList.toggle("clicked")
     });
     
-
-
     return box;
-
-
 };
 //** Grid generator */
 function
-generateGrid (boxContainer , difficulty) {
+generateGrid (boxContainer , difficulty ,oldContainerClass) {
     //* calc della radice del value della diffioclta
     let x = Math.sqrt(difficulty)
 
-    //* creo la classe da usare per quella diffioclta 
-    let y = "container-" + x ;
+    //* rimuovo la classe precedente se ce
+    boxContainer.classList.remove(oldContainerClass);
 
+    //* creo la classe da usare per quella diffioclta 
+    let newContainerClass = "container-" + x ;
+    
     //* aggiungo la classe al boxContainer
-    boxContainer.classList.toggle(y)
+    boxContainer.classList.add(newContainerClass);
 
     //* aggingo l'el box nel boxContainer
     for (let i = 0; i < difficulty; i++) {
     const boxElement = boxGenerator(i+ 1);
     boxContainer.append(boxElement);
     };
+    oldContainerClass = newContainerClass;
+    return(oldContainerClass);
 };
