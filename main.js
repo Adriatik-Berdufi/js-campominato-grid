49b6bf9ef5e7ed4b68d26086ee37807b5fb112b2
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-19 15:30:10 +0100

    feat:add generazione delle bombe ,e fine gioco quando si clica una bomba

--- main.js
+++ main.js
@@ -10,12 +10,16 @@ genera.addEventListener('click', function(){
 
     //* converto in intero il value della difficolta
     valueDifficulty = parseInt(difficulty.value);
+
+    //* call della funzione array con valori random 
+    arrayBMB =  random(valueDifficulty);
     
     //* call della funzione griglia 
-    oldContainerClass = generateGrid (boxContainer ,valueDifficulty, oldContainerClass );
-
+    oldContainerClass = generateGrid (boxContainer ,valueDifficulty, oldContainerClass, arrayBMB  );
+   
+    
     //* call all funzione per generare le box
-    boxGenerator();
+    boxGenerator(valueDifficulty, arrayBMB);
     
     
 });
@@ -26,24 +30,62 @@ genera.addEventListener('click', function(){
 
 
 //***** FUNZIONI *****//
+//** 16 nr casuali senza ripetterli  */
+function random(range){
+   
+    let randomNr = [];
+    while(randomNr.length < 16){
+        x = Math.floor(Math.random() * range + 1);
+        //controllo se non e gia presente in tal caso agggiungo
+        xPresente = randomNr.includes(x);
+        if(!xPresente){randomNr.push(x);};
+    };   
+    return randomNr;
+    
+}; 
 
 //** box generator  */
-function boxGenerator(nr) {
+function boxGenerator(nr,bmb) {
     
     //* vreo l'el e aggiungo la classe box
     const box = document.createElement('div');
     box.classList.add ('box');
+    box.classList.add("unclicked");
+    box.innerHTML = nr;
+    
+  
+    
     
     box.addEventListener('click', function(){
-        box.classList.toggle("clicked");
-        box.innerHTML = nr;
+        if(nr == "bomba"){
+            box.classList.add("bomb");
+            box.innerHTML = nr;
+            const endGame = document.querySelectorAll('.box');
+    
+            endGame.forEach(function(element) {
+                element.classList.remove('unclicked');
+                if (element.innerHTML === "bomba") {
+                    element.classList.add('bomb');
+                } else {
+                    element.classList.add('punto');
+                };
+               
+            });
+        } else {
+            box.classList.add("punto");
+            console.log(nr);
+        }
+    
+        console.log(box.classList);
     });
+ 
+     
     
     return box;
 };
 //** Grid generator */
 
-function generateGrid (boxContainer , difficulty ,oldContainerClass) {
+function generateGrid (boxContainer , difficulty ,oldContainerClass ,bmb) {
     //* calc della radice del value della diffioclta
     let x = Math.sqrt(difficulty)
 
@@ -55,12 +97,26 @@ function generateGrid (boxContainer , difficulty ,oldContainerClass) {
     
     //* aggiungo la classe al boxContainer
     boxContainer.classList.add(newContainerClass);
-
+    console.log(bmb);
     //* aggingo l'el box nel boxContainer
-    for (let i = 0; i < difficulty; i++) {
-    const boxElement = boxGenerator(i+ 1);
+    for (let i = 1; i <= difficulty; i++) {
+        if(bmb.includes(i)){
+            a = "bomba";
+        }else{
+            a = 1;
+        };
+
+    const boxElement = boxGenerator(a);
     boxContainer.append(boxElement);
+
     };
     oldContainerClass = newContainerClass;
     return(oldContainerClass);
+    
 };
+
+
+
+
+
+
