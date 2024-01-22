dc43330d4d0eae984677489a9e2f17543790d461
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-22 07:56:17 +0100

    fix: some bugs quando si terminava il gioco

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const boxContainer = document.getElementById("boxContainer");
 const genera = document.getElementById("genera");
 const difficulty = document.getElementById("difficulty");
+const Finalscore = document.getElementById("score");
 let oldContainerClass;
 let valueDifficulty;
 let score = 0;
@@ -17,18 +18,11 @@ genera.addEventListener('click', function(){
     
     //* call della funzione griglia 
     oldContainerClass = generateGrid (boxContainer ,valueDifficulty, oldContainerClass, arrayBMB  );
-   
     
     //* call all funzione per generare le box
     boxGenerator(valueDifficulty, arrayBMB);
     
-    
 });
-
-
-
-
-
 
 //***** FUNZIONI *****//
 //** 16 nr casuali senza ripetterli  */
@@ -51,37 +45,41 @@ function boxGenerator(nr,bmb) {
     //* vreo l'el e aggiungo la classe box
     const box = document.createElement('div');
     box.classList.add ('box');
-    box.innerHTML = nr;
+    //box.innerHTML = nr;
     box.classList.add("unclicked");
   
-    
-    
+    //* sul click del box
     box.addEventListener('click', function(){
+       
        if(!gameOver){
             if(nr == "bomba"){
                 box.classList.add("bomb");
                 box.innerHTML = nr;
                 gameOver = true;
-                alert(`hai perso: il tuo punteggio: ${score} `);
-                
-                
+                alert(`hai perso: il tuo punteggio: ${score} `); 
+                 
             } else {
                 box.classList.add("punto");
+                box.innerHTML = nr;
                 score ++;
                 if(score == valueDifficulty - 16){
-                    alert('Non so come ma hai vinto XD');
+                    gameOver = true;
+                    alert('Non so come, ma hai vinto XD');
                 };
-        
-                
             };
+            Finalscore.innerHTML = `=> ${score} `;
        };
+       if(gameOver){
+            finepartita();
+            finepartitaScopriCampo(valueDifficulty, arrayBMB,);  
+       }
        
     });
-    
-     
      
     return box;
 };
+
+
 //** Grid generator */
 
 function generateGrid (boxContainer , difficulty ,oldContainerClass ,bmb) {
@@ -104,18 +102,33 @@ function generateGrid (boxContainer , difficulty ,oldContainerClass ,bmb) {
         }else{
             a = 1;
         };
-
     const boxElement = boxGenerator(a);
     boxContainer.append(boxElement);
-
     };
     oldContainerClass = newContainerClass;
-    return(oldContainerClass);
-    
+    return(oldContainerClass); 
 };
-
-
-
-
-
-
+//** per rimmuvere la classe unclicked a tutti i box */
+function finepartita(){
+    const endGame = document.querySelectorAll('.box');
+    endGame.forEach(function(element) {
+        element.classList.remove('unclicked');
+        element.classList.add('bomb');
+      
+    });
+};
+function finepartitaScopriCampo(nr,bmb){
+    boxContainer.innerHTML = " ";
+    for (let i = 1; i <= nr; i++) {
+        if(bmb.includes(i)){   
+            a = "bomba";
+            css = "bomb"    
+        }else{    
+            a = 1;
+            css = "punto"
+        };
+        boxContainer.innerHTML += `
+        <div class="box ${css}">${a}</div> 
+        `;
+    };  
+};
